ac02da1af93da982867a0668b57b4b85f09c8139
apexcon <73210703+apexcon@users.noreply.github.com>
2023-07-03 10:47:08 +0530

    Add files via upload

--- js/songData.js
+++ js/songData.js
@@ -1,18 +1,18 @@
 const _albums = {
     "fukakouryoku": {
         name: "不可幸力",
-        artist: "Fukakouryoku - Vaundy",
+        artist: "Vaundy",
         audio: "audio/Fukakouryoku.mp3",
         coverArt: "cover/Fukakouryoku.jpeg",
     },
     "securitymeeting": {
-        name: "SecurityMeeting",
-        artist: "James Newton Howard",
+        name: "Security Meeting",
+        artist: "James Newton",
         audio: "audio/SecurityMeeting.mp3",
         coverArt: "cover/SecurityMeeting.jpeg",
     },
     "office": {
-        name: "The Office Theme",
+        name: "The Office",
         artist: "Jay Ferguson",
         audio: "audio/Office.mp3",
         coverArt: "cover/Office.jpeg",
